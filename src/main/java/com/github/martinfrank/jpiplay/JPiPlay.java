38ae9cd83d91322b5e847607380e858e86b52b8c
martinFrank <martin.frank.privat@googlemail.com>
2020-04-01 15:09:38 +0200

    initial setup

--- src/main/java/com/github/martinfrank/jpiplay/JPiPlay.java
+++ src/main/java/com/github/martinfrank/jpiplay/JPiPlay.java
@@ -1,46 +1,56 @@
 package com.github.martinfrank.jpiplay;
 
-import com.pi4j.io.gpio.*;
-
-import java.io.IOException;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.wiringpi.Gpio;
 
 public class JPiPlay {
 
     private final GpioController gpio;
 
+    private static final int PWM_RANGE = 1000;
+    private static final int PWM_CLOCK = 500;
+
     public static void main(String[] args) {
-        try {
-            System.out.println("main...");
-            JPiPlay play = new JPiPlay();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("main...");
+        new JPiPlay().start();
     }
 
 
-    public JPiPlay() throws IOException {
+    public JPiPlay() {
         gpio = GpioFactory.getInstance();
+        Gpio.pwmSetMode(Gpio.PWM_MODE_BAL);
+        Gpio.pwmSetRange(PWM_RANGE);
+        Gpio.pwmSetClock(PWM_CLOCK);
+    }
 
-        final GpioPinDigitalOutput myPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01,   // PIN NUMBER
-                "PIN01",           // PIN FRIENDLY NAME (optional)
-                PinState.LOW);      // PIN STARTUP STATE (optional)
+    public void start() {
+
+        final GpioPinPwmOutput pwm = gpio.provisionPwmOutputPin(RaspiPin.GPIO_23);
         new Thread(new Runnable() {
 
-            private boolean toogle = false;
             @Override
             public void run() {
-                for(int i = 0; i < 11; i++){
+                for (int i = 0; i < 11; i++) {
                     try {
-                        System.out.println("tick");
-                        myPin.setState(toogle);
-                        toogle = !toogle;
+                        double d = 0d + i / 10d;
+                        int range = (int) (d * PWM_RANGE);
+                        System.out.println("tick - range: " + range);
+                        pwm.setPwm(range);
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
+                shutDown();
             }
         }).start();
+    }
+
+    private void shutDown() {
+        gpio.shutdown();
     }
 
 
